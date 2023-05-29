718ec9a496741d259d30235c6509068f90de470d
tobi <tobibags19@gmail.com>
2023-05-29 12:05:43 -0500

    Implement css

--- flask_app.py
+++ flask_app.py
@@ -42,14 +42,13 @@ class CaloriesForm(Form):
     weight = StringField("Weight: ", default=70)
     height = StringField("Height: ", default=175)
     age = StringField("Age: ", default=32)
-    temperature = StringField("Temperature: ", default=75)
     city = StringField("City: ", default="dallas")
-    country = StringField("Country: ", default="usa")
+    country = StringField("Country: ", default="USA")
 
     button = SubmitField("Calculate")
 
 
 app.add_url_rule('/', view_func=HomePage.as_view('home_page'))
-app.add_url_rule('/calories_form_page', view_func=CaloriesFormPage.as_view('calories_form_page'))
+app.add_url_rule('/calories_form', view_func=CaloriesFormPage.as_view('calories_form_page'))
 
 app.run(debug=True)
